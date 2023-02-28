ac885e65ab2e48e944e0fadc88e10af520bf9002
jakobeha <jakobeha@gmail.com>
2023-02-28 17:09:47 -0500

    ok, NOW initial commit and planned skeleton
    - I actually have a lot of stuff at https://github.com/Jakobeha/nominal-js.git, which I am going to copy into this repository (nominal-js is a bare-bones TypeScript implementation, may go back to that if the Rust version is not working out)

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@ pub mod ast;
 pub mod import_export;
 /// Utilities which could go in any crate
 pub mod misc;
+/// This is the file with the one function (+ wrappers) which transpiles everything.
+///
+/// It really is just one massive function.
+/// Maybe I will end up refactoring out a lot of the functionality like I've already done for the
+/// type analysis and scope resolution, so it will be less massive.
+/// But honestly, it's just a really straightforward algorithm, and has a lot of cases to handle
+/// for all of JS/TS's different expressions, but all of the cases are really straightforward
+/// (with the less straightforward stuff in those other modules)
+pub mod compile;
 
 /// Run the program
 fn main() {
